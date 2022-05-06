f60384c13b8e230b27a988458a9c13093c51a223
Rodrigo Carreira <rcarreir@itba.edu.ar>
2022-05-06 15:49:14 +0200

    My first backend API

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+
+let notes = [
+  {
+    id: 1,
+    content: 'Me tengo que suscribir a @midudev en YouTube ',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Tengo que estudiar las clases del FullStack Bootcamp',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'Repasar los retos de JS de midudev',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Bienvenidos a mi sitio</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing'
+    })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date().toISOString,
+    important: typeof note.important !== 'undefined' ? note.important : false
+  }
+
+  notes = [...notes, newNote]
+
+  response.json(newNote)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
